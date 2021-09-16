a5bbf5f57077eddec48959c419e256d182b4b078
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-16 11:57:14 -0300

    Adição do exercício 20 feito no Windows 11.

---
+++ Exercicio20.py
@@ -0,0 +1,21 @@
+import random
+import emoji
+
+print('=====================================')
+print('           CHALLENGE 20')
+print('=====================================')
+print(emoji.emojize(':star: SORTEIO DE TRABALHOS A SEREM APRESENTADOS :star:', use_aliases=True))
+
+print('Digite o nome dos quatro alunos a seguir e obtenha a ordem a de apresentação.')
+a1 = str(input('Digite o nome do primeiro aluno: '))
+a2 = str(input('Digite o nome do segundo aluno: '))
+a3 = str(input('Digite o nome do terceiro aluno: '))
+a4 = str(input('Digite o nome do quarto aluno: '))
+
+print('\nOs trabalhos serão apresentados na seguinte ordem: ')
+print(f'Primeiro:{}')
+print(f'Segundo:{}')
+print(f'Terceiro:{}')
+print(f'Quarto:{}')
+
+NÃO RESOLVIDO
