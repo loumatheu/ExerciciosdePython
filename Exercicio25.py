16adb6a6b58f763a57da90096dbeffd9d794a9d2
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-21 13:09:35 -0300

    Atualização

--- Exercicio25.py
+++ Exercicio25.py
@@ -1,4 +1,7 @@
+import emoji
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
-nome = str(input('Olá! Qual o seu nome? '))
-if nome.find('Silva'):
-    print('Você tem um belíssimo nome e ele possue a palavra Silva!')
+nome = str(input('Olá! Qual o seu nome? ')).strip()
+if ('Silva' in nome):
+    print(emoji.emojize('Você tem um belíssimo nome e ele possue a palavra Silva!:satisfied:', use_aliases=True))
+else:
+    print(emoji.emojize('Infelizmente o seu nome não tem Silva :cry:', use_aliases=True))
