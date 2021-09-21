2705e01617b44cfa9643409db901d03a811ee9d0
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-21 09:43:45 -0300

    Conclusão do exercício 23

--- Exercicio23.py
+++ Exercicio23.py
@@ -4,3 +4,8 @@
 #dezena:3
 #centena:8
 #milhar:1
+n = input('Digite um número entre 0 e 9999: ')
+print(f'Unidade: {n[3]}')
+print(f'Dezena: {n[2]}')
+print(f'Centena: {n[1]}')
+print(f'Milhar: {n[0]}')
