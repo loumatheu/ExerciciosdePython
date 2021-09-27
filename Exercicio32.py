507ee22dad00c7f4265449de765baeae51912295
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-27 14:16:49 -0300

    Ex 32

--- Exercicio32.py
+++ Exercicio32.py
@@ -1,1 +1,10 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+year = int(input('Insira o ano: '))
+biyear = year % 4
+
+if biyear == 0:
+    print(f'O ano {year} é bissexto!')
+else:
+    print(f'O ano de {year} não é bissexto!')
+
+print('Exute o programa novamente para consultar outro ano!')
