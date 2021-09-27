75d8e4645a14f154e92fa749d482a0f454ab8e83
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-27 14:05:20 -0300

    Ex 31

---
+++ Exercicio31.py
@@ -0,0 +1,10 @@
+#Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$0,50
+#por km para viagens de até 200km e R$0,45 para viagens mais longas.
+distance = int(input('Insira a distância a ser percorrida em Km: '))
+
+if distance <= 200:
+    payment = distance * 0.5
+    print(f'Em uma distância de {distance}Km o valor a ser pago é: R${payment}')
+else:
+    payment = distance * 0.45
+    print(f'Em uma distância de {distance}Km valor a ser pago é: R${payment}')
---
+++ Exercicio35.py
@@ -0,0 +1,1 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se eles podem ou não forma um triângulo.
