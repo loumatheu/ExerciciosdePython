94a61d03f896732536d035e4228cf313f980475d
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-27 14:29:01 -0300

    Ex 33

--- Exercicio33.py
+++ Exercicio33.py
@@ -1,1 +1,23 @@
 #Faça um programa que leia 3 números e mostre qual é o maior e qual é o menor.
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
+
+if n1 > n2 > n3:
+    print(f'O maior número é {n1} e o menor número é {n3}!')
+else:
+    if n1 > n3 > n2:
+        print(f'O maior número é {n1} e o menor número é {n2}!')
+    else:
+        if n2 > n1 > n3:
+            print(f'O maior número é {n2} e o menor número é {n3}!')
+        else:
+            if n2 > n3 > n1:
+                print(f'O maior número é {n2} e o menor número é {n1}!')
+            else:
+                if n3 > n2 > n1:
+                    print(f'O maior número é {n3} e o menor número é {n1}!')
+                else:
+                    print(f'O maior número é {n3} e o menor número é {n2}!')
+
+print('Execute o programa novamente e insira outros números!')
