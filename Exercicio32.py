dd39794c58bcceb229c2be738485d7caeffc4f8f
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-27 21:40:39 -0300

    ex 32 att

--- Exercicio32.py
+++ Exercicio32.py
@@ -1,7 +1,11 @@
+from datetime import date
 #Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
-year = int(input('Insira o ano: '))
+year = int(input('Coloque zero para analisar o ano atual ou insira o ano desejado: '))
 
-if year % 4 == 0 and year % 100 != 0 or year % 4 == 0:
+if year == 0:
+    year = date.today().year
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print(f'O ano {year} é bissexto!')
 else:
     print(f'O ano de {year} não é bissexto!')
