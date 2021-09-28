228f07da9b011213cde0f6b49453435e479316e1
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-27 21:09:41 -0300

    ex 28 att

--- Exercicio28.py
+++ Exercicio28.py
@@ -1,12 +1,17 @@
-import random
+from random import randint
+from time import sleep
 #Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5. Peça para o usuário tentar
 #descobrir qual foi o número escolhido pelo computador.
 #O programa deverá escrever na tela se o usuário venceu ou perdeu.
 print("""===========================================================================================================
                                           JOGO DA ADVINHAÇÃO
 ===========================================================================================================""")
+print('Em qual número estou pensando?')
 numero = int(input('Escolha um número entre 0 e 5: '))
-pensado = random.randint(0, 5)
+pensado = randint(0, 5)
+print('Processando...')
+
+sleep(2)
 
 if numero == pensado:
     print(f'Você me pegou! O número que eu estava pensando é {pensado}!')
