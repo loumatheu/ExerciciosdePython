ff5887d8a8abae83bc6b2541cc4fe9f6194cdb30
loumatheu <luiz.matheus.77985@gmail.com>
2021-09-29 17:06:10 -0300

    Conclusão de exs mundo 1

--- Exercicio33.py
+++ Exercicio33.py
@@ -4,20 +4,20 @@ n2 = float(input('Digite o segundo número: '))
 n3 = float(input('Digite o terceiro número: '))
 
 if n1 > n2 > n3:
-    print(f'O maior número é {n1} e o menor número é {n3}!')
+    print(f'\nO maior número é {n1} e o menor número é {n3}!')
 else:
     if n1 > n3 > n2:
-        print(f'O maior número é {n1} e o menor número é {n2}!')
+        print(f'\nO maior número é {n1} e o menor número é {n2}!')
     else:
         if n2 > n1 > n3:
-            print(f'O maior número é {n2} e o menor número é {n3}!')
+            print(f'\nO maior número é {n2} e o menor número é {n3}!')
         else:
             if n2 > n3 > n1:
-                print(f'O maior número é {n2} e o menor número é {n1}!')
+                print(f'\nO maior número é {n2} e o menor número é {n1}!')
             else:
                 if n3 > n2 > n1:
-                    print(f'O maior número é {n3} e o menor número é {n1}!')
+                    print(f'\nO maior número é {n3} e o menor número é {n1}!')
                 else:
-                    print(f'O maior número é {n3} e o menor número é {n2}!')
+                    print(f'\nO maior número é {n3} e o menor número é {n2}!')
 
-print('Execute o programa novamente e insira outros números!')
+print('\nExecute o programa novamente e insira outros números!')
