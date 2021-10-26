4b7608d8dcf55586dbd5e4bcd4f04eeee35dd9f3
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:41:48 -0300

    Att adicionando cores

--- Mundo 1/Exercicio27.py
+++ Mundo 1/Exercicio27.py
@@ -2,6 +2,13 @@
 #ex: Ana Maria de Souza
 # primeiro = Ana
 # último = Souza
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 27
+===================================================================={cores['semestilo']}""")
+
 nome = input('Qual o seu nome? ').strip()
 lista = nome.split()
 print(f'Prazer em conhecê-la(o)! \nO seu primeiro nome é {lista[0]} e o seu último nome é {lista[-1]}.')
