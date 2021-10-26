d0d7696022582b76e2d1682e7aad72d7fd15732d
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:39:41 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio14.py
+++ Mundo 1/Exercicio14.py
@@ -1,13 +1,19 @@
-print('                  CHALLENGE 14')
-print('='*11, 'Conversor de Temperatura', '='*11)
-print('-'*50)
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 14
+===================================================================={cores['semestilo']}""")
+
+print(f'{cores["azul"]}                     Conversor de Temperatura')
+print(f'{cores["azul"]}-{cores["semestilo"]}'*67)
 C = float(input('Informe uma temperatura em ºC: '))
 F = (C*9/5)+32
-print(f'{C}ºC corresponde a {F}ºF. ')
+print(f'{cores["lilas"]}{C}ºC{cores["semestilo"]} corresponde a {cores["amarelo"]}{F}ºF{cores["semestilo"]}. ')
 if C <= 15:
-    print('Está congelando!')
+    print(f'{cores["azul"]}Está congelando!')
 elif C >= 27:
-    print('Está pegando fogo!')
+    print(f'{cores["vermelho"]}Está pegando fogo!')
 else:
-    print('A temperatura está agradável!')
-print('-'*50)
+    print(f'{cores["verde"]}A temperatura está agradável!')
+print(f'{cores["azul"]}-{cores["semestilo"]}'*67)
