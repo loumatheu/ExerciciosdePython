23f405fa1a78773839626f90c6fcb8db3ece434f
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:41:05 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio25.py
+++ Mundo 1/Exercicio25.py
@@ -1,5 +1,14 @@
 import emoji
+
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 25
+===================================================================={cores['semestilo']}""")
+
 nome = str(input('Olá! Qual o seu nome? ')).strip()
 if ('Silva' in nome):
     print(emoji.emojize('Você tem um belíssimo nome e ele possue a palavra Silva!:satisfied:', use_aliases=True))
