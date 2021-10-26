7d571bf9b6baa0f24df2081dab6ec746474ce832
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:41:32 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio26.py
+++ Mundo 1/Exercicio26.py
@@ -2,8 +2,15 @@
 # • Quantas vezes aparece a letra "A"
 # • Em que posição ela aparece a primeira vez.
 # • Em que posição ela paarece a última vez.
-nome = input('Qual o seu nome? ').strip()
 
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 26
+===================================================================={cores['semestilo']}""")
+
+nome = input('Qual o seu nome? ').strip()
 
 if 'a' in nome and 'A' in nome:
     print(f'A letra a aparece {(nome.upper().count("A"))} vezes no seu nome.')
