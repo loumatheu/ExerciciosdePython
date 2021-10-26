e7156d32267d5ea8ab4d7a8d055c2348f7ef33cc
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:39:56 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio16.py
+++ Mundo 1/Exercicio16.py
@@ -1,6 +1,12 @@
 from math import floor
-print('                 CHALLENGE 16')
-print('-'*50)
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 16
+===================================================================={cores['semestilo']}""")
+
+print(f'{cores["azul"]}-{cores["semestilo"]}'*67)
 num = float(input('Digite um número real e veja sua parte inteira: '))
 print(f'A parte inteira de {num} é {floor(num)}.')
-print('-'*50)
+print(f'{cores["azul"]}-{cores["semestilo"]}'*67)
--- Mundo 1/Exercicio17.py
+++ Mundo 1/Exercicio17.py
@@ -1,14 +1,19 @@
 import math
 import emoji
 
-print('=====================================')
-print('           CHALLENGE 17')
-print('=====================================')
-print(emoji.emojize(':star: CALCULANDO A HIPOTENUSA DE UM TRIÂNGULO RETANGULO :star:', use_aliases=True))
-catop = float(input('Digite o comprimento do cateto oposto: '))
-catadj = float(input('Digite o comprimento do cateto adjacente: '))
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 17
+===================================================================={cores['semestilo']}""")
+
+print(emoji.emojize(f'{cores["amarelo"]}:star:{cores["semestilo"]} CALCULANDO A HIPOTENUSA DE UM TRIÂNGULO RETANGULO '
+                    f'{cores["amarelo"]}:star:{cores["semestilo"]}', use_aliases=True))
+catop = float(input(f'Digite o comprimento do {cores["vermelho"]}cateto oposto{cores["semestilo"]}: '))
+catadj = float(input(f'Digite o comprimento do {cores["verdepiscina"]}cateto adjacente{cores["semestilo"]}: '))
 hip = math.pow(catop, 2) + math.pow(catadj, 2)
-print(f'A hipotenusa do triângulo em questão é: {math.sqrt(hip):.2f}')
+print(f'A hipotenusa do triângulo em questão é: {cores["verde"]}{math.sqrt(hip):.2f}')
 
 #Metódo mostrado na aula:
 #catetoop = float(input('Digite o comprimento do cateto oposto: '))
