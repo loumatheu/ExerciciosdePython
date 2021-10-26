08d980a6b60218e2a493671aafa61310f05a78db
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:40:46 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio24.py
+++ Mundo 1/Exercicio24.py
@@ -1,4 +1,11 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 24
+===================================================================={cores['semestilo']}""")
+
 cidade = str(input('Digite o nome de um cidade: ')).strip()
 
 if (cidade[:5] == 'Santo'):
