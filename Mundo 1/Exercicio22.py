fa8a9b96b9a31a9b7ee53c740c282de122d220b5
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:40:33 -0300

    Atualização dos exercícios

--- Mundo 1/Exercicio22.py
+++ Mundo 1/Exercicio22.py
@@ -3,6 +3,13 @@
 #•O nome com todas as letras minúsculas;
 #•Quantas letras ao todo (sem considerar espaços);
 #Quantas letras tem o primeiro nome;
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 22
+===================================================================={cores['semestilo']}""")
+
 nome = str(input('Qual o seu nome completo? ')).strip()
 print(f'Nome com todas as letras maiúsculas:{nome.upper()}')
 print(f'Nome com letras minúsculas:{nome.lower()}')
