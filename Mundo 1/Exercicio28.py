494831c560aaf4dccf8e8fd7b47e156feb30ba2f
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:41:54 -0300

    Att adicionando cores

--- Mundo 1/Exercicio28.py
+++ Mundo 1/Exercicio28.py
@@ -3,9 +3,17 @@ from time import sleep
 #Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5. Peça para o usuário tentar
 #descobrir qual foi o número escolhido pelo computador.
 #O programa deverá escrever na tela se o usuário venceu ou perdeu.
-print("""===========================================================================================================
-                                          JOGO DA ADVINHAÇÃO
-===========================================================================================================""")
+
+cores = {'azul':'\033[1;34m','verde':'\033[1;32m','semestilo':'\033[m', 'vermelho':'\033[1;31m',
+         'lilas':'\033[1;35m', 'amarelo':'\033[1;33m', 'verdepiscina':'\033[1;36m'}
+
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 28
+===================================================================={cores['semestilo']}""")
+
+print(f"""{cores['amarelo']}====================================================================
+                         JOGO DA ADVINHAÇÃO
+===================================================================={cores['semestilo']}""")
 print('Em qual número estou pensando?')
 numero = int(input('Escolha um número entre 0 e 5: '))
 pensado = randint(0, 5)
