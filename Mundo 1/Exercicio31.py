42546e39d3e6bc15eba97d76dc197a2046e50e65
loumatheu <luiz.matheus.77985@gmail.com>
2021-10-25 21:42:17 -0300

    Att adicionando cores

--- Mundo 1/Exercicio31.py
+++ Mundo 1/Exercicio31.py
@@ -1,5 +1,9 @@
 #Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$0,50
 #por km para viagens de até 200km e R$0,45 para viagens mais longas.
+print(f"""{cores['azul']}====================================================================
+                            CHALLENGE 31
+===================================================================={cores['semestilo']}""")
+
 distance = int(input('Insira a distância a ser percorrida em Km: '))
 
 if distance <= 200:
